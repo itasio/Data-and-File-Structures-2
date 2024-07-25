2e67e066fb1a034dfa5ee99c678f2900c1b482c4
itasio <61111336+itasio@users.noreply.github.com>
2024-07-25 18:46:38 +0300

    Modified MultiCounter to be thread safe.

--- src/counter/MultiCounter.java
+++ src/counter/MultiCounter.java
@@ -1,62 +1,70 @@
 package counter;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 /**
- * A class with static member variables and methods that can be used to count multiple stuff. 
+ * A class with static member variables and methods that can be used to count multiple stuff.
  * safe.
- * 
- * @author sk
+ *
  *
  */
 public class MultiCounter {
 
 	/**
-	 * variable holding our counters. We support up to 10 counters
+	 * Variable holding our counters. We support up to 10 counters
 	 */
-	private static int[] counters = new int[10];
-
+	private static final AtomicIntegerArray counters = new AtomicIntegerArray(10);
 
 	/**
 	 * Resets the internal counter of counterIndex to zero
+	 *
+	 * @return true if operation was successful otherwise false
 	 */
-	public static void resetCounter(int counterIndex) {
-		if (counterIndex-1 < counters.length)
-			counters[counterIndex-1] = 0;
+	public static boolean resetCounter(int counterIndex) {
+		if (counterIndex >= 1 && counterIndex-1 < counters.length()) {
+			counters.set(counterIndex-1, 0);
+			return true;
+		}
+		return false;
 	}
 
 	/**
 	 * Returns the current count for given counterIndex
-	 * 
-	 * @return the current count for given counterIndex
+	 *
+	 * @return the current count for given counterIndex or -1 if specified index is out of bounds
 	 */
 	public static int getCount(int counterIndex) {
-		if (counterIndex-1 < counters.length)
-			return counters[counterIndex-1];
+		if (counterIndex >= 1 && counterIndex-1 < counters.length())
+			return  counters.get(counterIndex-1);
 		return -1;
 	}
 
 	/**
 	 * Increases the current count of counterIndex by 1. Returns always true so that it can be used
 	 * in boolean statements
-	 * 
-	 * @return always true
+	 *
+	 * @return true if operation was successful otherwise false
 	 */
 	public static boolean increaseCounter(int counterIndex) {
-		if (counterIndex-1 < counters.length)
-			counters[counterIndex-1]++;
-		return true;
+		if (counterIndex >= 1 && counterIndex-1 < counters.length()) {
+			counters.incrementAndGet(counterIndex-1);
+			return true;
+		}
+		return false;
 	}
 
 	/**
 	 * Increases the current count of counter given by counterIndex by step. Returns always true so that it can be
 	 * used in boolean statements. Step could be negative. It is up to the specific
 	 * usage scenario whether this is desirable or not.
-	 * 
+	 *
 	 * @param step The amount to increase the counter
-	 * @return always true
+	 * @return true if operation was successful otherwise false
 	 */
 	public static boolean increaseCounter(int counterIndex, int step) {
-		if (counterIndex-1 < counters.length)
-			counters[counterIndex-1] = counters[counterIndex-1] + step;
-		return true;
+		if (counterIndex >= 1 && counterIndex-1 < counters.length()) {
+			counters.set(counterIndex - 1, counters.get(counterIndex - 1) + step);
+			return true;
+		}
+		return false;
 	}
 }
